f260cc0cf10266e24deddf6ffd5447e6e2195e5a
pavel.rebrov <p.rebrov@evrone.com>
2020-06-04 23:03:02 +0300

    Ad validates for User

--- app/model/user.rb
+++ app/model/user.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class User < Sequel::Model
+  NAME_FORMAT = %r{\A\w+\z}
+
+  def validate
+    super
+
+    validates_presence :email
+    validates_presence :name
+    validates_presence :password_digest
+    validates_format NAME_FORMAT, :name
+  end
 end
