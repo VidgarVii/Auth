53366e77586a01a7e2e30ef07dafd3930c830945
pavel.rebrov <p.rebrov@evrone.com>
2020-06-04 23:47:11 +0300

    Add schema db

---
+++ db/schema.rb
@@ -0,0 +1,18 @@
+Sequel.migration do
+  change do
+    create_table(:schema_migrations) do
+      column :filename, "text", :null=>false
+      
+      primary_key [:filename]
+    end
+    
+    create_table(:users) do
+      primary_key :id, :type=>:Bignum
+      column :name, "character varying", :null=>false
+      column :email, "character varying", :null=>false
+      column :password_digest, "character varying", :null=>false
+      column :created_at, "timestamp(6) without time zone", :null=>false
+      column :updated_at, "timestamp(6) without time zone", :null=>false
+    end
+  end
+end
