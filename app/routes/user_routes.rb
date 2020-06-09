45cac64d0615bef329bcc102f7de1940c31ef82e
pavel.rebrov <p.rebrov@evrone.com>
2020-06-09 08:28:08 +0300

    Fix route for UserRoutes

--- app/routes/user_routes.rb
+++ app/routes/user_routes.rb
@@ -3,17 +3,15 @@
 class UserRoutes < Application
   helpers Validations
 
-  namespace '/v1' do
-    post '/users' do
-      user_params = validate_with!(NewUserContract)
+  post '/users' do
+    user_params = validate_with!(NewUserContract)
 
-      result = Users::CreateService.call(user_params: user_params[:user])
+    result = Users::CreateService.call(user_params: user_params[:user])
 
-      if result.success?
-        status 201
-      else
-        error_response(result.user, 422)
-      end
+    if result.success?
+      status 201
+    else
+      error_response(result.user, 422)
     end
   end
 end
